e1ceb6a498eac4023483fad23117d6b24b87a5b1
Steven Kellner <steven.kellner@web.de>
2022-07-02 20:13:24 +0200

    Fixed date decoding strategy iso8601WithMilliseconds

--- Sources/Crypter/Extensions.swift
+++ Sources/Crypter/Extensions.swift
@@ -203,12 +203,23 @@ extension Array where Element == UInt8 {
     }
 }
 
-extension DateFormatter {
+extension JSONDecoder.DateDecodingStrategy {
 
-    /// Date formatter for iso8601 format with milliseconds
-    public static var iso8601WithMilliseconds: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        return formatter
+    /// Date decoding strategy for iso8601 format with milliseconds.
+    public static var iso8601WithMilliseconds: JSONDecoder.DateDecodingStrategy {
+        return JSONDecoder.DateDecodingStrategy.custom { decoder in
+            let container = try decoder.singleValueContainer()
+            let isoString = try container.decode(String.self)
+            let isoDateFormatter = ISO8601DateFormatter()
+            if let date = isoDateFormatter.date(from: isoString) {
+                return date
+            }
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+            if let date = dateFormatter.date(from: isoString) {
+                return date
+            }
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Couldn't decode iso8601 formatted date."))
+        }
     }
 }
